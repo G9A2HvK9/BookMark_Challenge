19ad07fa91205bc9b1b92c0576d33ff6f45f2dcc
hendrik <g9a2hvk9@me.com>
2017-05-03 10:37:37 +0100

    Refactors DatabaseCleaner to clean before suite and before each test, as well as after each test. Required & configured in spec_helper

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,19 +1,16 @@
 require "spec_helper"
-require "database_cleaner"
-DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.clean
 
 describe Link do
 
   feature 'viewing links' do
     scenario 'so I can see existing links on the home page' do
-      Link.create(url: 'https://www.makersacademy.com', name: 'Makers Academy')
+      Link.create(url: 'https://www.viewinglinks.com', name: 'Viewing Links Test')
 
       visit '/links'
       expect(page.status_code).to eq 200
 
       within 'ul#links' do
-        expect(page).to have_content('Makers Academy')
+        expect(page).to have_content('Viewing Links Test')
       end
     end
 
@@ -21,12 +18,12 @@ describe Link do
       scenario 'when I add a link on /links/new it will show up in my book marks' do
 
         visit '/links/new'
-        fill_in("name", :with => "People")
-        fill_in("url", :with => 'https://www.people.com')
+        fill_in("name", :with => "Adding Links Test")
+        fill_in("url", :with => 'https://www.addinglinks.com')
         click_button('Add Bookmark')
 
         within 'ul#links' do
-          expect(page).to have_content('https://www.people.com')
+          expect(page).to have_content('https://www.addinglinks.com')
 
         end
       end
